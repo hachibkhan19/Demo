324504b5daedfc1ab8b31ceeffbdf18cb919eda2
hachibkhan19 <hachib.khan19@gmail.com>
2022-10-28 18:13:55 +0600

    reverse relation in model

--- TrMIS/TrMIS/settings.py
+++ TrMIS/TrMIS/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'TrMISApp',
     'reverserelation',
+    'multiplemodelquery',
 ]
 
 MIDDLEWARE = [
---
+++ TrMIS/multiplemodelquery/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MultiplemodelqueryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'multiplemodelquery'
